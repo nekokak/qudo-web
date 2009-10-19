40a5677b4a424edac669c1bbe5e10897469a05e4
Atsushi Kobayashi <nekokak@gmail.com>
2009-10-19 22:26:26 +0900

    add methods.

--- lib/Qudo/Web/Controller/Root.pm
+++ lib/Qudo/Web/Controller/Root.pm
@@ -11,5 +11,20 @@ sub dispatch_job_list {
     $c->stash->{job_list} = container('qudo')->job_list;
 }
 
+sub dispatch_job_count {
+    my ($class, $c) = @_;
+    $c->stash->{job_count} = container('qudo')->job_count;
+}
+
+sub dispatch_job_status_list {
+    my ($class, $c) = @_;
+    $c->stash->{job_status_list} = container('qudo')->job_status_list;
+}
+
+sub dispatch_exception_list {
+    my ($class, $c) = @_;
+    $c->stash->{exception_list} = container('qudo')->exception_list;
+}
+
 1;
 
--- lib/Qudo/Web/Dispatcher.pm
+++ lib/Qudo/Web/Dispatcher.pm
@@ -9,5 +9,17 @@ on '/job_list' => run {
     return 'Root', 'job_list', FALSE, +{};
 };
 
+on '/job_count' => run {
+    return 'Root', 'job_count', FALSE, +{};
+};
+
+on '/job_status_list' => run {
+    return 'Root', 'job_status_list', FALSE, +{};
+};
+
+on '/exception_list' => run {
+    return 'Root', 'exception_list', FALSE, +{};
+};
+
 1;
 
